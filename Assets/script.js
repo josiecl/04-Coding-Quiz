079a37ff4b38e087a8eae79e3fdd1c662438cfd1
Josie <josiecollinsrc@gmail.com>
2021-01-13 22:35:18 -0600

    more CSS added, hyperlink to scoreboard made

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
 var questionNo = document.querySelector("#questions");
 var quizBox = document.querySelector("#quiz");
 var submitButton = document.querySelector("#submit");
+var scoreboard = document.querySelector("#highscore")
+
